79ef67723ded265503585d223b37e08f7716870e
mahdyabdnia <44471104+mahdyabdnia@users.noreply.github.com>
2020-08-26 20:19:52 +0430

    Add files via upload

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/custom.css">
 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/custom.js"></script>
+
     
 
     
@@ -26,51 +28,41 @@
 
 <input type="text" class="search-bar" placeholder="نام برند و کالا را وارد کنید " >
 <button class="search-btn btn btn-dark"><i class="fa fa-search"></i></button>
-
-<button type="button" class="btn btn-outline-dark shop-btn"><span class="badge badge-pill badge-dark" style="font-size: 12px">0</span> <span class="shop-text">سبد خرید</span> <em class="fa fa-shopping-cart shop-icon" style="font-size: 23px"></em></button>
+<?php $totalqty=0; ?>
+        <?php foreach($this->cart->contents() as $items): ?>
+          <?php $totalqty+=$items['qty']; ?>
+        <?php endforeach;?>
+<button  type="button" class="btn btn-outline-dark shop-btn"><a class="shop-link"href="<?php echo base_url(); ?>Cart"><span class="badge badge-pill badge-dark" style="font-size: 12px"><?php echo $totalqty; ?></span> <span class="shop-text">سبد خرید</span> <em class="fa fa-shopping-cart shop-icon" style="font-size: 23px"></em></a></button>
 <button type="button" id="btn-modal" data-toggle="modal" data-target="#myModal" class="btn btn-link bt-app1">اپلیکیشن دور بازار</button>
 
 <button class="btn btn-link bt-app2" id=tt>تولید کننده هستم</button>
-<button class="btn btn-link bt-app3" id="btn-modal2" data-target="myModal1" data-toggle="modal">ثبت نام -ورود</button>
+<?php if(isset($_SESSION['logged_in'])): ?>
+<button class="btn btn-link bt-app3" id="btn-modal2" data-target="myModal1" data-toggle="modal" ><a class="header-link" href="<?php echo base_url() ?>user/logout ">خروج</a> </button>
+
+<?php else: ?>  
+<button class="btn btn-link bt-app3" id="btn-modal2" data-target="myModal1" data-toggle="modal" ><a class="header-link" href="<?php echo base_url() ?>user ">ثبت نام -ورود </a> </button>
+<?php endif; ?>
 
 <script>
 
-$(document).ready(function(){
-  $("#btn-modal").click(function(){
-    $("#myModal").fadeIn();
-  });
-});
-
-</script>
-
-<script>
-$(document).ready(function(){
-  $("body").dblclick(function(){
-   $("#myModal").fadeOut();
-
-  });
-});
 
 
 </script>
 
 <script>
 
-$(document).ready(function(){
-  $("#btn-modal2").click(function(){
-    $("#myModal1").fadeIn();
-  });
-});
+
 
 </script>
 
 <script>
-$(document).ready(function(){
-$("body").dblclick(function(){
-$("#myModal1").fadeOut();
 
-});
-});
+
+
+</script>
+
+<script>
+
 
 
 </script>
@@ -130,22 +122,6 @@ $("#myModal1").fadeOut();
   </div>
   
 
-
-
-<br>
-<div>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<ul class="navbar-nav ml-auto">
-<li class="nav-item dropdown  " >
-<a class="nav-link  " href="#" id="navbardrop" > <i id="span-dop" class="fa fa-caret-down"></i>    مواد غذایی</a>
- <ul class="dropdown-menu" id="dropdown" style="text-align: right ;background: #343A40">
-      <li> <a class="dropdown-item" href="#"> <i class=" fa fa-angle-left"></i>  لینک1 </a></li> 
-        
-        
-     <li> <a class="dropdown-item" href="#">لینک2 </a></li>  
-     <li> <a class="dropdown-item" href="#">لینک3 </a></li>  
-      </ul>
-      
       <script>
       $(document).ready(function(){
       
@@ -156,46 +132,51 @@ $("#myModal1").fadeOut();
       
       
       </script>
-      
-      
-
-</li>
 
 
+<br>
+<div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<ul class="navbar-nav ml-auto">
 
-<li class="nav-item dropdown ">
-<a class="nav-link  " href="#" id="navbardrop" > <i id="span-dop" class="fa fa-caret-down"></i>    مواد غذایی</a>
+<?php foreach ($global_result as $globres ):?>
+
+<li class="nav-item dropdown  " >
+<a class="nav-link  " href="#" id="navbardrop" > <i id="span-dop" class="fa fa-caret-down"></i><?php echo $globres->global_category; ?></a>
  <ul class="dropdown-menu" id="dropdown" style="text-align: right ;background: #343A40">
-      <li> <a class="dropdown-item" href="#"> <i class=" fa fa-angle-left"></i>  لینک1 </a></li> 
-        
-        
-     <li> <a class="dropdown-item" href="#">لینک2 </a></li>  
-     <li> <a class="dropdown-item" href="#">لینک3 </a></li>  
-      </ul>
+      <li> <a class="dropdown-item" href="#"> <i class=" fa fa-angle-left"></i>  لینک1 </a>
+
+         <ul class="dropdown-menu submenu submenu-left" id="dropdown" style="text-align: right ;background: #343A40">
+            <li class=""><a class="dropdown-item" href="">مواد  غذایی</a></li>
+
+             <li class=""><a class="dropdown-item" href="">مواد  غذایی</a></li>
+              <li class=""><a class="dropdown-item" href="">مواد  غذایی</a></li>
+         </ul>
+       
+
+      </li> 
+
+
+      <li> <a class="dropdown-item" href="#"> <i class=" fa fa-angle-left"></i>  لینک1 </a>
+
+         <ul class="dropdown-menu submenu submenu-left
+         " id="dropdown" style="text-align: right ;background: #343A40">
+            <li class=""><a class="dropdown-item" href="">مواد  غذایی</a></li>
+         </ul>
+       
+
+      </li> 
+
+    
+    
+ 
+  </ul>
+   
 </li>
 
-<li class="nav-item dropdown ">
-<a class="nav-link  " href="#" id="navbardrop" > <i id="span-dop" class="fa fa-caret-down"></i>    مواد غذایی</a>
- <ul class="dropdown-menu" id="dropdown" style="text-align: right ;background: #343A40">
-      <li> <a class="dropdown-item" href="#"> <i class=" fa fa-angle-left"></i>  لینک1 </a></li> 
-        
-        
-     <li> <a class="dropdown-item" href="#">لینک2 </a></li>  
-     <li> <a class="dropdown-item" href="#">لینک3 </a></li>  
-      </ul>
-</li>
 
-<li class="nav-item dropdown ">
-<a class="nav-link  " href="#" id="navbardrop" > <i id="span-dop" class="fa fa-caret-down"></i>    مواد غذایی</a>
- <ul class="dropdown-menu" id="dropdown" style="text-align: right ;background: #343A40">
-      <li> <a class="dropdown-item" href="#"> <i class=" fa fa-angle-left"></i>  لینک1 </a></li> 
-        
-        
-     <li> <a class="dropdown-item" href="#">لینک2 </a></li>  
-     <li> <a class="dropdown-item" href="#">لینک3 </a></li>  
-      </ul>
-</li>
 
+<?php endforeach; ?>
 
 
 
